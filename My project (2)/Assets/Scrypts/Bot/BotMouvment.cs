4229de8294bd4ba39badc5c3a28d9d14f96e464c
takuryto2 <150046987+takuryto2@users.noreply.github.com>
2023-12-01 16:38:43 +0100

    bot mov

---
+++ My project (2)/Assets/Scrypts/Bot/BotMouvment.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//private enum state {avance, saute, tire }
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
